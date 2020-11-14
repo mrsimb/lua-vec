f6e3a0656a060799751477e5c88d1633e4e317ac
an.pavlov <an.pavlov@mt-innovations.ru>
2020-11-14 15:40:27 +0300

    Fix tabulation

--- vec.lua
+++ vec.lua
@@ -14,7 +14,7 @@ setmetatable(Vec, Vec.protomt)
 -- @return Vec
 -- @see Vec.from
 function Vec.protomt:__call(x, y, z)
-	return Vec.from(x, y, z)
+  return Vec.from(x, y, z)
 end
 
 --- + operator
@@ -80,7 +80,7 @@ end
 -- @usage Vec(1, 2, 3) == Vec(1, 2, 3) -> true
 -- @see Vec:eq
 function Vec.mt:__eq(v)
-	return self:eq(v)
+  return self:eq(v)
 end
 
 --- < operator
@@ -89,7 +89,7 @@ end
 -- @usage Vec(1, 2, 3) < Vec(3, 3, 3) -> true
 -- @see Vec:lt
 function Vec.mt:__lt(v)
-	return self:lt(v)
+  return self:lt(v)
 end
 
 --- <= operator
@@ -98,7 +98,7 @@ end
 -- @usage Vec(1, 2, 3) <= Vec(3, 3, 3) -> true
 -- @see Vec:lt
 function Vec.mt:__le(v)
-	return self:le(v)
+  return self:le(v)
 end
 
 --- .. operator
@@ -114,7 +114,7 @@ end
 -- @return string
 -- @usage print(Vec(1, 2, 3)) -> '{1, 2, 3}'
 function Vec.mt:__tostring()
-	return self:toString()
+  return self:toString()
 end
 
 --- Sets vector components to input. Accepts numbers, table or Vec
@@ -125,25 +125,25 @@ end
 -- @usage Vec(1, 2, 3):set(10) -> {10, 2, 3}
 -- @usage Vec(1, 2, 3):set(nil, nil, 10) -> {1, 2, 10}
 function Vec:set(x, y, z)
-	local v = Vec.from(x or self.x, y or self.y, z or self.z)
-	self.x = v.x
-	self.y = v.y
-	self.z = v.z
-	return self
+  local v = Vec.from(x or self.x, y or self.y, z or self.z)
+  self.x = v.x
+  self.y = v.y
+  self.z = v.z
+  return self
 end
 
 --- Returns string representation of vector
 -- @return string
 -- @usage Vec(1, 2, 3):toString() -> '{1, 2, 3}'
 function Vec:toString()
-	return '{' .. self.x .. ', ' .. self.y .. ', ' .. self.z .. '}'
+  return '{' .. self.x .. ', ' .. self.y .. ', ' .. self.z .. '}'
 end
 
 --- Returns array of vector components
 -- @return table
 -- @usage Vec(1, 2, 3):toArray() -> {1, 2, 3}
 function Vec:toArray()
-	return {self.x, self.y, self.z}
+  return {self.x, self.y, self.z}
 end
 
 --- Sets vector component by numeric index
@@ -184,7 +184,7 @@ end
 -- @usage Vec(1, 2, 3):add(10, 10, 10) -> {11, 12, 13}
 function Vec:add(x, y, z)
   local v = Vec.from(x or 0, y or 0, z or 0)
-	return Vec.from(self.x + v.x, self.y + v.y, self.z + v.z)
+  return Vec.from(self.x + v.x, self.y + v.y, self.z + v.z)
 end
 
 --- Returns vectors difference
@@ -196,7 +196,7 @@ end
 -- @usage Vec(1, 2, 3):sub(10, 10, 10) -> {-9, -8, -7}
 function Vec:sub(x, y, z)
   local v = Vec.from(x or 0, y or 0, z or 0)
-	return Vec.from(self.x - v.x, self.y - v.y, self.z - v.z)
+  return Vec.from(self.x - v.x, self.y - v.y, self.z - v.z)
 end
 
 --- Returns vector whose components are multiplicated by input
@@ -208,7 +208,7 @@ end
 -- @usage Vec(1, 2, 3):mul(10, 10, 10) -> {10, 20, 30}
 function Vec:mul(x, y, z)
   local v = Vec.from(x or 1, y or 1, z or 1)
-	return Vec.from(self.x * v.x, self.y * v.y, self.z * v.z)
+  return Vec.from(self.x * v.x, self.y * v.y, self.z * v.z)
 end
 
 --- Returns vector whose components are divided by input
@@ -219,8 +219,8 @@ end
 -- @usage Vec(1, 2, 3):div(10) -> {0.1, 2, 3}
 -- @usage Vec(1, 2, 3):div(10, 10, 10) -> {0.1, 0.2, 0.3}
 function Vec:div(x, y, z)
-	local v = Vec.from(x or 1, y or 1, z or 1)
-	return Vec.from(self.x / v.x, self.y / v.y, self.z / v.z)
+  local v = Vec.from(x or 1, y or 1, z or 1)
+  return Vec.from(self.x / v.x, self.y / v.y, self.z / v.z)
 end
 
 --- Returns vector whose components are raised by input
@@ -232,7 +232,7 @@ end
 -- @usage Vec(2, 3, 4):pow(3, 3, 3) -> {8, 27, 64}
 function Vec:pow(x, y, z)
   local v = Vec.from(x or 1, y or 1, z or 1)
-	return Vec.from(self.x ^ v.x, self.y ^ v.y, self.z ^ v.z)
+  return Vec.from(self.x ^ v.x, self.y ^ v.y, self.z ^ v.z)
 end
 
 --- Returns true if vector and input are equal
@@ -243,7 +243,7 @@ end
 -- @usage Vec(2, 3, 4):eq(2, 3, 4) -> true
 function Vec:eq(x, y, z)
   local v = Vec.from(x, y, z)
-	return self.x == v.x and self.y == v.y and self.z == v.z
+  return self.x == v.x and self.y == v.y and self.z == v.z
 end
 
 --- Returns true if vector magnitude is less than input
@@ -253,7 +253,7 @@ end
 -- @return Vec
 -- @usage Vec(2, 3, 4):lt(10, 10, 10) -> true
 function Vec:lt(x, y, z)
-	return self:len() < Vec.from(x, y, z):len()
+  return self:len() < Vec.from(x, y, z):len()
 end
 
 --- Returns true if vector magnitude is less than or equal to input
@@ -263,7 +263,7 @@ end
 -- @return boolean
 -- @usage Vec(2, 3, 4):le(10, 10, 10) -> true
 function Vec:le(x, y, z)
-	return self:len() <= Vec.from(x, y, z):len()
+  return self:len() <= Vec.from(x, y, z):len()
 end
 
 --- Returns true if vector magnitude is greater than input
@@ -273,7 +273,7 @@ end
 -- @return boolean
 -- @usage Vec(2, 3, 4):gt(0, 0, 0) -> true
 function Vec:gt(x, y, z)
-	return self:len() > Vec.from(x, y, z):len()
+  return self:len() > Vec.from(x, y, z):len()
 end
 
 --- Returns true if vector magnitude is greater than or equal to input
@@ -283,28 +283,28 @@ end
 -- @return boolean
 -- @usage Vec(2, 3, 4):ge(0, 0, 0) -> true
 function Vec:ge(x, y, z)
-	return self:len() >= Vec.from(x, y, z):len()
+  return self:len() >= Vec.from(x, y, z):len()
 end
 
 --- Returns vector magnitude (length)
 -- @return number
 -- @usage Vec(10, 0, 0):len() -> 10
 function Vec:len()
-	return math.sqrt(self:lenSq())
+  return math.sqrt(self:lenSq())
 end
 
 --- Returns vector magnitude (length) squared
 -- @return number
 -- @usage Vec(10, 0, 0):len() -> 100
 function Vec:lenSq()
-	return self.x ^ 2 + self.y ^ 2 + self.z ^ 2
+  return self.x ^ 2 + self.y ^ 2 + self.z ^ 2
 end
 
 --- Returns distance beetween vectors
 -- @return number
 -- @usage Vec(10, 0, 0):dist(15, 0, 0) -> 5
 function Vec:dist(x, y, z)
-	return math.sqrt(self:distSq(x, y, z))
+  return math.sqrt(self:distSq(x, y, z))
 end
 
 --- Returns distance squared between vectors
@@ -312,7 +312,7 @@ end
 -- @usage Vec(10, 0, 0):distSq(15, 0, 0) -> 25
 function Vec:distSq(x, y, z)
   local v = Vec.from(x, y, z)
-	return self:sub(v):lenSq()
+  return self:sub(v):lenSq()
 end
 
 --- Returns vectors cross product
@@ -322,8 +322,8 @@ end
 -- @return Vec
 -- @usage Vec(3, -3, 1):cross(4, 9, 2) -> {-15, -2, 39}
 function Vec:cross(x, y, z)
-	local v = Vec.from(x, y, z)
-	return Vec.from(
+  local v = Vec.from(x, y, z)
+  return Vec.from(
     self.y * v.z - self.z * v.y,
     self.z * v.x - self.x * v.z,
     self.x * v.y - self.y * v.x
@@ -338,7 +338,7 @@ end
 -- @usage Vec(1, 2, 3):cross(6, 7, 8) -> 44
 function Vec:dot(x, y, z)
   local v = Vec.from(x, y, z)
-	return self.x * v.x + self.y * v.y + self.z * v.z
+  return self.x * v.x + self.y * v.y + self.z * v.z
 end
 
 --- Returns normalized vector
@@ -354,14 +354,14 @@ end
 -- @return Vec
 -- @usage Vec(10, 5, 0):limit(2) -> {0.97014250014533, 0.24253562503633, 0}
 function Vec:limit(n)
-	return self:norm():mul(n)
+  return self:norm():mul(n)
 end
 
 --- Returns 2D rotation angle of vector
 -- @return number
 -- @usage Vec(-1, 0, 0):angle2d() -> 3.1415926535898
 function Vec:angle2d()
-	return math.atan2(self.y, self.x)
+  return math.atan2(self.y, self.x)
 end
 
 --- Returns angle between 3D vectors
@@ -370,20 +370,20 @@ end
 -- @param z number
 -- @return number
 function Vec:angleBetween(x, y, z)
-	local v = Vec.from(x, y, z)
-	return math.acos(self:dot(v) / (self:len() * v:len()))
+  local v = Vec.from(x, y, z)
+  return math.acos(self:dot(v) / (self:len() * v:len()))
 end
 
 --- Returns vector with 2D rotation applied to it
 -- @param a number
 -- @return Vec
 function Vec:rotate2d(a)
-	local v = Vec.from(self)
-	local na = self:angle2d() + a
-	local m = self:len()
-	v.x = math.cos(na) * m
-	v.y = math.sin(na) * m
-	return v
+  local v = Vec.from(self)
+  local na = self:angle2d() + a
+  local m = self:len()
+  v.x = math.cos(na) * m
+  v.y = math.sin(na) * m
+  return v
 end
 
 --- Returns linear interpolation between vectors
@@ -395,11 +395,11 @@ end
 -- @usage Vec(10, -10, 2):lerp(0.5, {20, 10, 1}) -> {15, 0, 1.5}
 function Vec:lerp(n, x, y, z)
   local v = Vec.from(x, y, z)
-	return Vec.from(
-		self.x + (v.x - self.x) * n,
-		self.y + (v.y - self.y) * n,
-		self.z + (v.z - self.z) * n
-	)
+  return Vec.from(
+    self.x + (v.x - self.x) * n,
+    self.y + (v.y - self.y) * n,
+    self.z + (v.z - self.z) * n
+  )
 end
 
 -- Static methods
@@ -409,24 +409,24 @@ end
 -- @return Vec
 -- @usage Vec.fromAngle(0) -> {1, 0, 0}
 function Vec.fromAngle(a)
-	return Vec.from(math.cos(a or 0), math.sin(a or 0))
+  return Vec.from(math.cos(a or 0), math.sin(a or 0))
 end
 
 --- Returns normalized vector from random 2D angle
 -- @return Vec
 function Vec.random2d()
-	local a = math.random() * math.pi * 2;
-	return Vec.fromAngle(a)
+  local a = math.random() * math.pi * 2;
+  return Vec.fromAngle(a)
 end
 
 --- Returns normalized random vector
 -- @return Vec
 function Vec.random()
-	return Vec(
-		math.random() * 2 - 1,
-		math.random() * 2 - 1,
-		math.random() * 2 - 1
-	):norm()
+  return Vec(
+    math.random() * 2 - 1,
+    math.random() * 2 - 1,
+    math.random() * 2 - 1
+  ):norm()
 end
 
 --- Constructs new vector. Call Vec() directly to achieve same result
@@ -439,17 +439,17 @@ end
 -- @usage Vec({x = 1, y = 0, z = 0})
 -- @usage Vec(anotherVector)
 function Vec.from(x, y, z)
-	if (type(x) == 'table') then
-	  return Vec.from(x.x or x[1], x.y or x[2], x.z or x[3])
-	end
-	
-	local v = {}
+  if (type(x) == 'table') then
+    return Vec.from(x.x or x[1], x.y or x[2], x.z or x[3])
+  end
+  
+  local v = {}
 
-	v.x = x or 0
-	v.y = y or 0
+  v.x = x or 0
+  v.y = y or 0
   v.z = z or 0
   
   setmetatable(v, Vec.mt)
 
-	return v
+  return v
 end
